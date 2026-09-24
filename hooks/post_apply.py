363d88a5370d46409fcf9dd98fa6cca87e0d9c58
Jean-Francois Chevrette <jchevret@redhat.com>
2026-09-24 16:00:28 -0400

    fix: include private dns name in removal logs

--- hooks/post_apply.py
+++ hooks/post_apply.py
@@ -44,19 +44,26 @@ class VpcEndpointServicePostApply:
         configurations = response.get("ServiceConfigurations", [])
         if not configurations:
             raise RuntimeError(f"VPC Endpoint Service {service_id} was not found")
-        if not configurations[0].get("PrivateDnsName"):
+        private_dns_name = configurations[0].get("PrivateDnsName")
+        if not private_dns_name:
             return
 
         if self.config.dry_run:
             logger.info(
-                "Would remove private DNS name from VPC Endpoint Service %s", service_id
+                "Would remove private DNS name %s from VPC Endpoint Service %s",
+                private_dns_name,
+                service_id,
             )
             return
 
         ec2.modify_vpc_endpoint_service_configuration(
             ServiceId=service_id, RemovePrivateDnsName=True
         )
-        logger.info("Removed private DNS name from VPC Endpoint Service %s", service_id)
+        logger.info(
+            "Removed private DNS name %s from VPC Endpoint Service %s",
+            private_dns_name,
+            service_id,
+        )
         sys.exit(1)
 
 
